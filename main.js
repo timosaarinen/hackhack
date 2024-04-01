b0ca462b5b110aed1c0411c0410daa45f653729b
Timo Saarinen <tsaari42@gmail.com>
2024-04-01 10:45:15 +0300

    Deleted old main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-import * as THREE from 'three';
-
-console.log("Hack & Hack - XR Tabletop Role-Playing Game (XRTRPG) - Copyright (c) 2024 Timo Saarinen")
-
-const scene = new THREE.Scene()
-
-// ..and so it begins.
